4d408c037b45b0a68dfb9f48853094efce401e95
Charlie Coleman <coleman.charlie97@gmail.com>
2023-03-08 00:02:11 -0600

    rework atfive to a class with better statistic storage

--- api/api.py
+++ api/api.py
@@ -2,40 +2,21 @@ import flask
 from flask_cors import CORS, cross_origin
 import uuid
 from pathlib import Path
-import os
 import pytz
 import json
 import argparse
-import threading
 import datetime as dt
-from util.twitch import TwitchAPI
-from util.twitch import TWITCH_API_TIME_FORMAT
+from luscioustwitch import *
 from util.at_five import *
 
-
 class API_DataStore():
-  TWITCH_API = None
-  LOCAL_TZ = None
-  DATA_TIMEOUT = 120
-
-  RESULTS_FILEPATH = "./results.csv"
-  RESULTS = dict()
-  IS_LIVE = False
-  RESULTS_AGE = None
+  ATFIVE_API = None
 
 data = API_DataStore()
 
 app = flask.Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = 'Content-Type'
-
-def update_results():
-  data.RESULTS = get_at_five_results(data.TWITCH_API, data.RESULTS_FILEPATH, data.LOCAL_TZ)
-  save_at_five_results(data.RESULTS, data.RESULTS_FILEPATH)
-  
-  data.IS_LIVE = data.TWITCH_API.get_broadcaster_live()
-
-  data.RESULTS_AGE = dt.datetime.now()
 
 @app.route('/', methods=['GET'])
 @cross_origin()
@@ -45,12 +26,10 @@ def home():
 @app.route('/api/v1/record', methods=['GET'])
 @cross_origin()
 def get_record():
-  if (data.RESULTS_AGE is None) or ((dt.datetime.now() - data.RESULTS_AGE).total_seconds() > data.DATA_TIMEOUT):
-    update_results()
-  o, e, t = calc_record(data.RESULTS)
-  status, streak = get_current_streak(data.RESULTS)
+  o, e, t = data.ATFIVE_API.calc_record()
+  status, streak = data.ATFIVE_API.get_current_streak()
   if 'plaintext' in flask.request.args:
-    return_str = f"{get_when_live(data)} He has been early {e} times, on time {o} times, and late {t-o-e} times."
+    return_str = f"{data.ATFIVE_API.get_when_live()} He has been early {e} times, on time {o} times, and late {t-o-e} times."
     if streak > 1:
       status_str = status.name.lower()
       return_str += f" He has been {status_str} {streak} streams in a row."
@@ -62,21 +41,20 @@ def get_record():
       'early': e,
       'total': t,
       'streak': streak,
-      'streak-status': status.value
+      'streak-status': status
     }
     return flask.jsonify(resp)
 
 @app.route('/api/v1/history', methods=['GET'])
 @cross_origin()
 def get_history():
-  if (data.RESULTS_AGE is None) or ((dt.datetime.now() - data.RESULTS_AGE).total_seconds() > data.DATA_TIMEOUT):
-    update_results()
-
+  data.ATFIVE_API.update_data_if_necessary()
+  
   streams = dict()
-  for k, v in data.RESULTS.items():
+  for k, v in data.ATFIVE_API.STATS.items():
     streams[k] = dict()
-    streams[k]["on_time"] = v[0].value
-    streams[k]["time"] = v[1].isoformat()
+    streams[k]["on_time"] = v["on-time"]
+    streams[k]["time"] = dt.datetime.strptime(v['datetime'], TWITCH_API_TIME_FORMAT).strftime("%H:%M:%S")
 
   resp = { 'streams': streams }
 
@@ -85,11 +63,9 @@ def get_history():
 @app.route('/api/v1/live', methods=['GET'])
 @cross_origin()
 def get_live():
-  if (data.RESULTS_AGE is None) or ((dt.datetime.now() - data.RESULTS_AGE).total_seconds() > data.DATA_TIMEOUT):
-    update_results()
+  data.ATFIVE_API.update_data_if_necessary()
     
-  todays_date = utc_to_local(dt.datetime.now(), data.LOCAL_TZ).date()
-  resp = { 'live' : 1 if data.IS_LIVE else 0, 'waslive' : 1 if todays_date.strftime("%Y-%m-%d") in data.RESULTS else 0 }
+  resp = { 'live' : 1 if data.ATFIVE_API.IS_LIVE else 0, 'waslive' : 1 if data.ATFIVE_API.WAS_LIVE else 0 }
   return flask.jsonify(resp)
 
 if __name__ == '__main__':
@@ -99,21 +75,12 @@ if __name__ == '__main__':
   parser.add_argument('--channel', '-c', default="itswill", help="Twitch channel name")
   parser.add_argument('--secrets', '-s', default="./secrets.json", help="JSON file with credentials")
   parser.add_argument('--timezone', '-t', default="America/Los_Angeles", help="Local timezone")
-  parser.add_argument('--results', '-r', default="./results/results.csv", help="File to keep results in.")
+  parser.add_argument('--datapath', '-d', default="./data", help="File to keep results in.")
   
   args = parser.parse_args()
 
-  data.LOCAL_TZ = pytz.timezone(args.timezone)
-  data.RESULTS_FILEPATH = args.results
-
   with open(args.secrets) as cred_file:
     cred_data = json.load(cred_file)
-    data.TWITCH_API = TwitchAPI(cred_data["TWITCH"])
-
-  channel_found = data.TWITCH_API.get_broadcaster_id(args.channel, set_default=True)
-  if not channel_found:
-    print(f"Failed to find {args.channel} in Twitch directory.")
-
-  update_results()
+    data.ATFIVE_API = AtFiveAPI(TwitchAPI(cred_data["TWITCH"]), creator=args.channel, tz=pytz.timezone(args.timezone))
 
   app.run(host=args.host, port=args.port, debug=True)
--- api/util/at_five.py
+++ api/util/at_five.py
@@ -12,31 +12,15 @@ import datetime as dt
 import pytz
 from string import Template
 from enum import Enum
-from util.twitch import TwitchAPI
-from util.twitch import TWITCH_API_TIME_FORMAT
+from luscioustwitch import *
 
 CSV_DELIM = ','
 CSV_QUOTE = '|'
 
-GOAL_TIME = dt.time(17, 0, 0)
-BUFFER_MIN = 15
-
-ONTIME_START = (dt.datetime.combine(dt.date.today(), GOAL_TIME) - dt.timedelta(minutes=BUFFER_MIN)).time()
-ONTIME_END = (dt.datetime.combine(dt.date.today(), GOAL_TIME) + dt.timedelta(minutes=BUFFER_MIN)).time()
-
-class Punctuality(Enum):
-  LATE=0
-  ONTIME=1
-  EARLY=2
-  def __lt__(self, other):
-    if self.__class__ is other.__class__:
-      return self.value < other.value
-    return NotImplemented
-
 class DeltaTemplate(Template):
     delimiter = "%"
 
-def strfdelta(tdelta, fmt):
+def strfdelta(tdelta : dt.timedelta, fmt):
     d = {"D": tdelta.days}
     hours, rem = divmod(tdelta.seconds, 3600)
     _, hours_12hr = divmod(hours, 12)
@@ -51,7 +35,7 @@ def strfdelta(tdelta, fmt):
     t = DeltaTemplate(fmt)
     return t.substitute(**d)
   
-def smartfmtdelta(tdelta):
+def smartfmtdelta(tdelta : dt.timedelta):
   hours, rem = divmod(tdelta.seconds, 3600)
   minutes, _ = divmod(rem, 60)
   
@@ -62,104 +46,200 @@ def smartfmtdelta(tdelta):
   else:
     return strfdelta(tdelta, "%{s}s")
 
-def utc_to_local(utc_dt, local_tz):
+def utc_to_local(utc_dt : dt.datetime, local_tz):
   return utc_dt.replace(tzinfo=dt.timezone.utc).astimezone(tz=local_tz)
 
-def timeadd(time1, time2):
+def timeadd(time1 : dt.time, time2 : dt.time):
   return (dt.datetime.combine(dt.date.today(), time1) + dt.datetime.combine(dt.date.today(), time2))
 
-def timediff(time1, time2):
+def timediff(time1 : dt.time, time2: dt.time):
   return (dt.datetime.combine(dt.date.today(), time1) - dt.datetime.combine(dt.date.today(), time2))
 
-def is_five(start_time):
-  if start_time < ONTIME_START:
-    return Punctuality.EARLY
-  elif start_time >= ONTIME_START and start_time <= ONTIME_END: 
-    return Punctuality.ONTIME
-  else:
-    return Punctuality.LATE
-
-def this_year(vod_date):
-  return vod_date >= dt.date(2023, 1, 1) and vod_date <= dt.date(2023, 12, 31)
-
-def get_at_five_results(twitch_api, results_file, local_tz):
-  os.makedirs(os.path.dirname(results_file), exist_ok=True)
-  Path(results_file).touch()
-
-  at_five_results = dict()
-
-  with open(results_file, 'r', newline='', encoding='utf-8') as resultscsv:
-    results_reader = csv.reader(resultscsv, delimiter=CSV_DELIM, quotechar=CSV_QUOTE)
-    for row in results_reader:
-      at_five_results[row[0]] = (Punctuality(int(row[1])), dt.datetime.strptime(row[2], "%H:%M:%S").time())
-
-  vods = twitch_api.get_all_videos()
-  for vod in vods:
-    vod_dt = utc_to_local(dt.datetime.strptime(vod["created_at"], TWITCH_API_TIME_FORMAT), local_tz)
-    if this_year(vod_dt.date()):
-      date_str = str(vod_dt.date())
-      vod_time = vod_dt.time()
-      if date_str in at_five_results:
-        curr_val = at_five_results[date_str][0]
-        new_val = is_five(vod_time)
-        at_five_results[date_str] = (curr_val if new_val < curr_val else new_val, vod_time)
-      else:
-        at_five_results[date_str] = (is_five(vod_time), vod_time)
-
-  return at_five_results
-
-def save_at_five_results(results, results_file):
-  with open(results_file, 'w', newline='', encoding='utf-8') as resultscsv:
-    results_writer = csv.writer(resultscsv, delimiter=CSV_DELIM, quotechar=CSV_QUOTE, quoting=csv.QUOTE_MINIMAL)
-    for k, v in results.items():
-      results_writer.writerow([k, v[0].value, v[1]])
-
-def calc_record(results):
-  ontime = 0
-  early = 0
-  total = 0
-  for d, r in results.items():
-    total += 1
-    if r[0] == Punctuality.ONTIME:
-      ontime += 1
-    elif r[0] == Punctuality.EARLY:
-      early += 1
-  return (ontime, early, total)
-
-def get_current_streak(results):
-  status = Punctuality.EARLY
-  streak = 0
-
-  stream_dates = list(results.keys())
-  stream_dates.sort(reverse = True)
+class Punctuality(int, Enum):
+  LATE   = 0
+  ONTIME = 1
+  EARLY  = 2
+  def __lt__(self, other):
+    if self.__class__ is other.__class__:
+      return self.value < other.value
+    return NotImplemented
   
-  status = results[stream_dates[0]][0]
-  for stream in stream_dates:
-    if status == results[stream][0]:
-      streak += 1
+class AtFiveAPI():
+  TWITCH_API : TwitchAPI = None
+  DATA_PATH  : Path      = Path("./data")
+  LOCAL_TZ               = pytz.timezone("America/Los_Angeles")
+  VIDEO_FILE : Path      = DATA_PATH / "videos.json"
+  VIDEOS     : dict      = { "list": [], "count": 0 }
+  STATS_FILE : Path      = DATA_PATH / "stats.json"
+  STATS      : dict      = {}
+  
+  USER_ID = ""
+  
+  GOAL_TIME = dt.time(17, 0, 0)
+  BUFFER_MIN = 15
+  
+  IS_LIVE  = False
+  WAS_LIVE = False
+  
+  DATA_TIMESTAMP = dt.datetime.now()
+  DATA_TIMEOUT   = 60
+  
+  def __init__(self, twitch_api : TwitchAPI, datapath : Path = Path("./data"), tz = pytz.timezone("America/Los_Angeles"), creator = "itswill", start_time : dt.time = dt.time(17, 0, 0), buffer : int = 15):
+    self.TWITCH_API = twitch_api
+    self.DATA_PATH  = datapath if isinstance(datapath, Path) else Path(datapath)
+    self.LOCAL_TZ   = tz
+    
+    os.makedirs(self.DATA_PATH, exist_ok = True)
+    
+    self.VIDEO_FILE = self.DATA_PATH / "videos.json"
+    self.VIDEO_FILE.touch()
+    self.STATS_FILE = self.DATA_PATH / "stats.json"
+    self.STATS_FILE.touch()
+
+    self.ONTIME_START = (dt.datetime.combine(dt.date.today(), self.GOAL_TIME) - dt.timedelta(minutes=self.BUFFER_MIN)).time()
+    self.ONTIME_END = (dt.datetime.combine(dt.date.today(), self.GOAL_TIME) + dt.timedelta(minutes=self.BUFFER_MIN)).time()
+    
+    self.USER_ID = self.TWITCH_API.get_user_id(creator)
+    if self.USER_ID == "":
+      print(f"Failed to find {creator} in Twitch directory.")
+      
+    self._read_archived_data()
+    self._fetch_twitch_data()
+    self._save_data()
+    
+    self._check_is_live()
+    self._check_was_live()
+    
+  def update_results(self):
+    self._fetch_twitch_data()
+    self._save_data()
+    self._check_is_live()
+    self._check_was_live()
+    
+    self.DATA_TIMESTAMP = dt.datetime.now()
+    
+  def update_data_if_necessary(self):
+    if (self.DATA_TIMESTAMP is None) or ((dt.datetime.now() - self.DATA_TIMESTAMP).total_seconds() > self.DATA_TIMEOUT):
+      self.update_results()
+
+  def _is_five(self, start_time : dt.time):
+    if start_time < self.ONTIME_START:
+      return Punctuality.EARLY
+    elif start_time >= self.ONTIME_START and start_time <= self.ONTIME_END: 
+      return Punctuality.ONTIME
     else:
-      break
+      return Punctuality.LATE
 
-  return (status, streak)
+  def _this_year(self, vod_date):
+    return vod_date >= dt.date(2023, 1, 1) and vod_date <= dt.date(2023, 12, 31)
+  
+  def _load_json(self, filepath, default = {}):
+    with open(filepath, 'r') as jsonfile:
+      try:
+        return json.load(jsonfile)
+      except:
+        return default
+      
+  def _sort_data(self):
+    self.STATS = dict(sorted(self.STATS.items(), reverse=True))
+    self.VIDEOS["list"] = sorted(self.VIDEOS["list"], key=lambda x: dt.datetime.strptime(x["published_at"], TWITCH_API_TIME_FORMAT), reverse=True)
+    self.VIDEOS["count"] = len(self.VIDEOS["list"])
+    
+  def _read_archived_data(self):
+    self.VIDEOS = self._load_json(self.VIDEO_FILE, { "list": [], "count": 0 })
+    self.STATS  = self._load_json(self.STATS_FILE)
+    self._sort_data()
 
-def get_when_live(data):
-    now = utc_to_local(dt.datetime.now(tz=dt.timezone.utc), data.LOCAL_TZ)
+  def _fetch_twitch_data(self):
+    video_params = {
+      "user_id": self.USER_ID,
+      "period": "all",
+      "sort": "time",
+      "type": "archive"
+    }
+    vods = self.TWITCH_API.get_all_videos(video_params)
+    
+    for vod in vods:
+      vod_dt = utc_to_local(dt.datetime.strptime(vod["created_at"], TWITCH_API_TIME_FORMAT), self.LOCAL_TZ)
+      if self._this_year(vod_dt.date()):
+        if vod not in self.VIDEOS["list"]:
+          self.VIDEOS["list"].append(vod)
+          
+        vod_date = vod_dt.date().isoformat()
+        if vod_date not in self.STATS or self.LOCAL_TZ.localize(dt.datetime.strptime(self.STATS[vod_date]["datetime"], TWITCH_API_TIME_FORMAT), is_dst=None) > vod_dt:
+          td = timediff(vod_dt.time(), self.GOAL_TIME)
+          offset_seconds = td.seconds + (24.0*60.0*60.0*td.days)
+          self.STATS[vod_date] = {
+            "offset": (offset_seconds / 60.0),
+            "on-time": self._is_five(vod_dt.time()).value,
+            "datetime": vod_dt.strftime(TWITCH_API_TIME_FORMAT)
+          }
+        
+    self._sort_data()
+    
+  def _save_data(self):
+    with open(self.VIDEO_FILE, 'w') as videofile:
+      videofile.write(json.dumps(self.VIDEOS, indent=2))
+    with open(self.STATS_FILE, 'w') as statsfile:
+      statsfile.write(json.dumps(self.STATS, indent=2))
+  
+  def _check_is_live(self):
+    self.update_data_if_necessary()
+    
+    self.IS_LIVE = self.TWITCH_API.is_broadcaster_live(self.USER_ID)
+    
+  def _check_was_live(self):
+    now = utc_to_local(dt.datetime.now(tz=dt.timezone.utc), self.LOCAL_TZ)
+    self.WAS_LIVE = (now.strftime("%Y-%m-%d") in self.STATS)
+
+  def calc_record(self):
+    self.update_data_if_necessary()
+    
+    ontime = 0
+    early = 0
+    total = 0
+    for _, stats in self.STATS.items():
+      total += 1
+      if stats["on-time"] == Punctuality.ONTIME:
+        ontime += 1
+      elif stats["on-time"] == Punctuality.EARLY:
+        early += 1
+    return (ontime, early, total)
+
+  def get_current_streak(self):
+    self.update_data_if_necessary()
+    
+    status = 0
+    streak = 0
+    
+    for __, stats in self.STATS.items():
+      if streak == 0:
+        status = stats["on-time"]
+      if status == stats["on-time"]:
+        streak += 1
+      else:
+        break
+
+    return (status, streak)
+
+  def get_when_live(self):
+    now = utc_to_local(dt.datetime.now(tz=dt.timezone.utc), self.LOCAL_TZ)
     whenLive = ""
     
-    if data.IS_LIVE:
+    if self.IS_LIVE:
       whenLive = "itswill is live now."
-    elif now.strftime("%Y-%m-%d") in data.RESULTS:
+    elif self.WAS_LIVE:
       whenLive = "Today's stream has ended."
     elif now.weekday() == 0:
       whenLive = "Probably no stream today BoyHowdyISureDoHateMondays ."
-    elif now.time() < GOAL_TIME:
-      timeStr = smartfmtdelta(timediff(GOAL_TIME, now.time()))
+    elif now.time() < self.GOAL_TIME:
+      timeStr = smartfmtdelta(timediff(self.GOAL_TIME, now.time()))
       whenLive = f"itswill should be live in {timeStr}."
-    elif now.time() <= ONTIME_END:
-      timeStr = smartfmtdelta(timediff(ONTIME_END, now.time()))
+    elif now.time() <= self.ONTIME_END:
+      timeStr = smartfmtdelta(timediff(self.ONTIME_END, now.time()))
       whenLive = f"Respect the 15 minute buffer. He still has {timeStr} left."
     else:
-      timeStr = smartfmtdelta(timediff(now.time(), ONTIME_END))
+      timeStr = smartfmtdelta(timediff(now.time(), self.ONTIME_END))
       whenLive = f"itswill should've been live {timeStr} ago."
     
     return whenLive
